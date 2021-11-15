981df1144d5addbd81f2dd3cb01119ecf6e02688
Jack1Player <chricg99@gmail.com>
2021-11-15 12:24:50 +0100

    evergreen: specify screen details

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -79,3 +79,8 @@ TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
+
+# Evergreen details
+# Resolution
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
